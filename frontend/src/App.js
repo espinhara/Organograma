d407632d0b7d86577c83fa1e8bd57ed919654af8
espinhara <gabrielespinhara13@gmail.com>
2024-06-10 06:26:07 -0300

    feat: ✨ Use nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,13 @@ import { Routes, Route, Link } from 'react-router-dom';
 import OrgChart from './OrgChart';
 import AddEmployee from './AddEmployee';
 import EditEmployee from './EditEmployee';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Organograma</h1>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/add">Add Employee</Link>
-      </nav>
+      <Nav />
       <Routes>
         <Route path="/" element={<OrgChart />} />
         <Route path="/add" element={<AddEmployee />} />
