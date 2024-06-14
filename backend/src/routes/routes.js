b87c6bdcb41d95bfc02c25d560be339798e703bd
espinhara <gabrielespinhara13@gmail.com>
2024-06-14 01:37:13 -0300

    feat: ✨ added router to upload photo

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../controllers/employeeController');
+const multer = require('multer');
+const upload = multer({ dest: 'temp/' });
 
 // Adicione esta rota
+router.post('/upload', upload.single('photo'), employeeController.uploadPhoto);
 router.get('/', employeeController.getAllEmployees);
 router.get('/:id', employeeController.getEmployeeById);
 router.post('/', employeeController.addEmployee);
